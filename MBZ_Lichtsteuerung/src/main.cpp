7f11769bd4bfcb88b488900012b3ac55690e3292
k3vinio96 <72357033+k3vinio96@users.noreply.github.com>
2024-04-22 22:55:05 +0200

    erster Testaufbau

--- MBZ_Lichtsteuerung/src/main.cpp
+++ MBZ_Lichtsteuerung/src/main.cpp
@@ -1,18 +1,56 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+void toggle_output(int);
+int sec = 1000;
+int act_millis;
+int last_millis_1 = 0;
+int last_millis_2 = 0;
+int last_millis_4 = 0;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  pinMode(16, OUTPUT); // D0
+  pinMode(5, OUTPUT);  // D1
+  pinMode(4, OUTPUT);  // D2
+  pinMode(0, OUTPUT);  // D3
+
+  digitalWrite(16, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
+void loop()
+{
+  
   // put your main code here, to run repeatedly:
+  act_millis = millis();
+
+  if (act_millis - last_millis_1 > sec)
+  {
+    toggle_output(5);
+    last_millis_1 = act_millis;
+  }
+  if (act_millis - last_millis_2 > sec*2)
+  {
+    toggle_output(4);
+    last_millis_2 = act_millis;
+  }
+  if (act_millis - last_millis_4 > sec*4)
+  {
+    toggle_output(0);
+    last_millis_4 = act_millis;
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void toggle_output(int pin)
+{
+  if (digitalRead(pin) == HIGH)
+    digitalWrite(pin, LOW);
+  else
+    digitalWrite(pin, HIGH);
 }
