02aee9982267da11376cfe5867a438a157b5642f
k3vinio96 <kevinseidel96@gmx.de>
2024-06-03 17:47:30 +0200

    Erweiterung auf 8 Relays

--- MBZ_Lichtsteuerung/include/html/indexPage.h
+++ MBZ_Lichtsteuerung/include/html/indexPage.h
@@ -14,7 +14,14 @@ const char indexPage[] = R"=====(
             <a class="btn btn-primary disabled" href="/" role="button">Home</a>
         </p>
 
-        <form action="/LED" method="POST"><input type="submit" value="Toggle LED"></form>
+        <form action="/Relay1" method="POST"><input type="submit" value="Toggle Relay 1"></form>
+        <form action="/Relay2" method="POST"><input type="submit" value="Toggle Relay 2"></form>
+        <form action="/Relay3" method="POST"><input type="submit" value="Toggle Relay 3"></form>
+        <form action="/Relay4" method="POST"><input type="submit" value="Toggle Relay 4"></form>
+        <form action="/Relay5" method="POST"><input type="submit" value="Toggle Relay 5"></form>
+        <form action="/Relay6" method="POST"><input type="submit" value="Toggle Relay 6"></form>
+        <form action="/Relay7" method="POST"><input type="submit" value="Toggle Relay 7"></form>
+        <form action="/Relay8" method="POST"><input type="submit" value="Toggle Relay 8"></form>
 
         <p>Copyright (C) 2024 --- MBZ "Saxonia" Oberlungwitz e.V.</a>.</p>
     </div>
--- MBZ_Lichtsteuerung/src/main.cpp
+++ MBZ_Lichtsteuerung/src/main.cpp
@@ -9,13 +9,29 @@ void toggle_output(int);
 void StartAccessPoint(const char *, const char *);
 void ServerHandler();
 void handleRoot();
-void handleLED();
+void handleRelay1();
+void handleRelay2();
+void handleRelay3();
+void handleRelay4();
+void handleRelay5();
+void handleRelay6();
+void handleRelay7();
+void handleRelay8();
 
 int sec = 1000;
 int act_millis;
 int last_millis_1 = 0;
 int last_millis_2 = 0;
 int last_millis_4 = 0;
+
+int Relay_1 = 16; //D0
+int Relay_2 = 5;  //D1
+int Relay_3 = 4;  //D2
+int Relay_4 = 0;  //D3
+int Relay_5 = 2;  //D4
+int Relay_6 = 14; //D5
+int Relay_7 = 12; //D6
+int Relay_8 = 13; //D7
 
 const char *ssid = "MBZ_Licht";
 const char *password = "LangenLicht";
@@ -28,15 +44,14 @@ void setup()
 {
   // put your setup code here, to run once:
 
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(16, OUTPUT); // D0
-  pinMode(5, OUTPUT);  // D1
-  pinMode(4, OUTPUT);  // D2
-  pinMode(0, OUTPUT);  // D3
-
-  digitalWrite(16, HIGH);
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(Relay_1, OUTPUT);
+  pinMode(Relay_2, OUTPUT); 
+  pinMode(Relay_3, OUTPUT); 
+  pinMode(Relay_4, OUTPUT);  
+  pinMode(Relay_5, OUTPUT); 
+  pinMode(Relay_6, OUTPUT); 
+  pinMode(Relay_7, OUTPUT); 
+  pinMode(Relay_8, OUTPUT);
 
   StartAccessPoint(ssid, password);
   ServerHandler();
@@ -49,23 +64,6 @@ void loop()
   act_millis = millis();
 
   server.handleClient();
-
-
-  if (act_millis - last_millis_1 > sec)
-  {
-    toggle_output(5);
-    last_millis_1 = act_millis;
-  }
-  if (act_millis - last_millis_2 > sec * 2)
-  {
-    toggle_output(4);
-    last_millis_2 = act_millis;
-  }
-  if (act_millis - last_millis_4 > sec * 4)
-  {
-    toggle_output(0);
-    last_millis_4 = act_millis;
-  }
 }
 
 // put function definitions here:
@@ -86,7 +84,15 @@ void StartAccessPoint(const char *ssid, const char *password)
 void ServerHandler()
 {
   server.on("/", HTTP_GET, handleRoot);
-  server.on("/LED", HTTP_POST, handleLED);
+
+  server.on("/Relay1", HTTP_POST, handleRelay1);
+  server.on("/Relay2", HTTP_POST, handleRelay2);
+  server.on("/Relay3", HTTP_POST, handleRelay3);
+  server.on("/Relay4", HTTP_POST, handleRelay4);
+  server.on("/Relay5", HTTP_POST, handleRelay5);
+  server.on("/Relay6", HTTP_POST, handleRelay6);
+  server.on("/Relay7", HTTP_POST, handleRelay7);
+  server.on("/Relay8", HTTP_POST, handleRelay8);
 }
 
 void handleRoot()
@@ -95,9 +101,58 @@ void handleRoot()
   server.send(200, "text/html", indexPage);
 }
 
-void handleLED()
+void handleRelay1()
 {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); 
+  digitalWrite(Relay_1, !digitalRead(Relay_1)); 
+  server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleRelay2()
+{
+  digitalWrite(Relay_2, !digitalRead(Relay_2)); 
+  server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleRelay3()
+{
+  digitalWrite(Relay_3, !digitalRead(Relay_3)); 
+  server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleRelay4()
+{
+  digitalWrite(Relay_4, !digitalRead(Relay_4)); 
+  server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleRelay5()
+{
+  digitalWrite(Relay_5, !digitalRead(Relay_5)); 
+  server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleRelay6()
+{
+  digitalWrite(Relay_6, !digitalRead(Relay_6)); 
+  server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleRelay7()
+{
+  digitalWrite(Relay_7, !digitalRead(Relay_7)); 
+  server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+}
+
+void handleRelay8()
+{
+  digitalWrite(Relay_8, !digitalRead(Relay_8)); 
   server.sendHeader("Location", "/");   // Add a header to respond with a new location for the browser to go to the home page again
   server.send(303);                     // Send it back to the browser with an HTTP status 303 (See Other) to redirect
 }
